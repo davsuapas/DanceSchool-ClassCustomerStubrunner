8ed1352782bcf8566965c0359abd53ed775aa054
dav.sua.pas@gmail.com <dav.sua.pas@gmail.com>
2019-01-05 17:34:02 +0100

    configure stubmode

--- src/main/java/com/elipcero/classcustomerstubrunnerschool/ClassCustomerStubrunnerSchoolApplication.java
+++ src/main/java/com/elipcero/classcustomerstubrunnerschool/ClassCustomerStubrunnerSchoolApplication.java
@@ -3,9 +3,11 @@ package com.elipcero.classcustomerstubrunnerschool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.contract.stubrunner.server.EnableStubRunnerServer;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 
 @SpringBootApplication
 @EnableStubRunnerServer
+@AutoConfigureStubRunner
 public class ClassCustomerStubrunnerSchoolApplication {
 
 	public static void main(String[] args) {
