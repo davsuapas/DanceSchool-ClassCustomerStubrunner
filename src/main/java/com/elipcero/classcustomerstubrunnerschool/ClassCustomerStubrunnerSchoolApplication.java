d9f4eee2cd3710e8ad96b23e71cbb83ce1c688f0
David Suarez <dav.sua.pas@gmail.com>
2019-01-08 17:25:00 +0100

    test

--- src/main/java/com/elipcero/classcustomerstubrunnerschool/ClassCustomerStubrunnerSchoolApplication.java
+++ src/main/java/com/elipcero/classcustomerstubrunnerschool/ClassCustomerStubrunnerSchoolApplication.java
@@ -1,8 +1,15 @@
 package com.elipcero.classcustomerstubrunnerschool;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.contract.stubrunner.server.EnableStubRunnerServer;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableStubRunnerServer
@@ -12,4 +19,21 @@ public class ClassCustomerStubrunnerSchoolApplication {
 		SpringApplication.run(ClassCustomerStubrunnerSchoolApplication.class, args);
 	}
 
+	@Component
+	private static class Init implements CommandLineRunner {
+
+		private final Logger logger = LoggerFactory.getLogger(Init.class);
+
+		@Override
+		public void run(String... args) throws Exception {
+			try {
+				String address = InetAddress.getByName("artifactory").getHostAddress();
+				this.logger.info("--------------------------> Address:" + address);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
+
+
