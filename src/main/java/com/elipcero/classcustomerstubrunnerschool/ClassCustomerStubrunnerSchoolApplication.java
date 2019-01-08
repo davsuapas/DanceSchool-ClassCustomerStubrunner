16c018f3488ea06e48d44f1f96afbb2891cd562f
David Suarez <dav.sua.pas@gmail.com>
2019-01-08 17:45:54 +0100

    test

--- src/main/java/com/elipcero/classcustomerstubrunnerschool/ClassCustomerStubrunnerSchoolApplication.java
+++ src/main/java/com/elipcero/classcustomerstubrunnerschool/ClassCustomerStubrunnerSchoolApplication.java
@@ -5,14 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.contract.stubrunner.server.EnableStubRunnerServer;
 import org.springframework.stereotype.Component;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @SpringBootApplication
-@EnableStubRunnerServer
+//@EnableStubRunnerServer
 public class ClassCustomerStubrunnerSchoolApplication {
 
 	public static void main(String[] args) {
