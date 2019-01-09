19a123dc7df0a50084c4378875d4be62d0400891
dav.sua.pas@gmail.com <dav.sua.pas@gmail.com>
2019-01-09 12:47:03 +0100

    configure srtream

--- src/main/java/com/elipcero/classcustomerstubrunnerschool/ClassCustomerStubrunnerSchoolApplication.java
+++ src/main/java/com/elipcero/classcustomerstubrunnerschool/ClassCustomerStubrunnerSchoolApplication.java
@@ -3,14 +3,20 @@ package com.elipcero.classcustomerstubrunnerschool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.contract.stubrunner.server.EnableStubRunnerServer;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
 @EnableStubRunnerServer
+@EnableBinding
 public class ClassCustomerStubrunnerSchoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClassCustomerStubrunnerSchoolApplication.class, args);
 	}
+
+	@AutoConfigureStubRunner
+	static class Config {}
 }
 
 
